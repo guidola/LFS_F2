ff855c0e7be974ca48230289cacc7d9cb78732f3
Guillermo Dobao <ga2468@hotmail.com>
2016-12-06 13:32:57 +0100

    added notification template

---
+++ www/js/templates/notification_template.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Guillermo on 6/12/16.
+ */
+
+new PNotify({
+    title: 'TITLE_PLACEHOLDER',
+    text: 'TEXT_PLACEHOLDER',
+    type: 'TYPE_PLACEHOLDER',
+    styling: 'bootstrap3'
+});
