a5a5d5001419fe8a52ea7787833e0d70d1a427e3
Adrià Mallorqui <ls28953@alleurl.edu>
2016-12-17 16:22:54 +0100

    Removed an echo that caused an invalid json response

--- cgi/iptables.sh
+++ cgi/iptables.sh
@@ -78,9 +78,9 @@ if [[ ! -z $resp_code ]]; then
             if [[ ! -z $ACTION ]]; then
                 echo '{"rc": true}'
             else
-                echo "Dobai is the JS king" >> "${ret_fifo}"
+                echo "Dobao is the JS king" >> "${ret_fifo}"
                 read response < $ret_fifo
-		        echo "second read done"
+		        #echo "second read done"
                 echo "{\"rc\":true, \"payload\": $response"
             fi
             ;;
