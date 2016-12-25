6e026af610fa197bbddec8cb9b9b978b96b2e5b6
Guillermo Dobao <ga2468@hotmail.com>
2016-12-25 23:57:25 +0100

    finished async load of side menu for logs. creating a menu structure as it is in folder structure on the system. Every log file has a link to logs.html but there's on them a path of the log corresponding that menu entry. Use that path to fill the log table with proper information. Going to work with only one column as the whole log since format is varying and kind of messy.

---
+++ cgi/getLogsAvailable.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# list the logs folder /var/log and create a json with all the .log files to show em as options on the side-menu
+
+lsToJson() {
+
+    first=1
+    echo "["
+
+    for node in *; do
+        if [[ $node == '.' || $node == '..' || "$node" == '*' ]]; then continue; fi
+
+        #if its a file .log put it on the json
+        if [[ -f ${node} ]]; then
+            if [[ ${node} =~ \.log$ ]]; then
+                if [[ $first -ne 1 ]]; then
+                    echo ", { \"type\": \"file\", \"name\": \"${node::${#node}-4}\", \"path\": \"`pwd`/${node}\" }"
+                else
+                    echo "{ \"type\": \"file\", \"name\": \"${node::${#node}-4}\", \"path\": \"`pwd`/${node}\" }"
+                    first=0
+                fi
+            else
+                last_node=""
+            fi
+        fi
+
+        #if its a folder call this func and put pre and post brackets for next json depth
+        if [[ -d ${node} ]]; then
+            cd ${node} 2>>/dev/null
+            if [[ $? -ne 0 ]]; then continue; fi
+            if [[ $first -ne 1 ]]; then
+                echo ","
+            else
+                first=0
+            fi
+            echo "{ \"type\": \"folder\", \"name\": \"${node}\", \"childs\": "
+            lsToJson
+            cd ..
+            echo "}"
+        fi
+
+    done;
+
+    echo "${last_node}]"
+    first=0
+}
+
+
+# enter /var/log to avoid static paths
+cd /var/log/
+
+# echo the header
+echo "Status: 200 Ok"
+echo "Content-Type: application/json"
+echo ""
+
+lsToJson
--- www/js/style.js
+++ www/js/style.js
@@ -2,20 +2,74 @@
  * Created by Guillermo on 30/10/16.
  */
 
+var log_file_li_template = document.createElement('li');
+log_file_li_template.setAttribute('data-role', 'log-trigger');
+log_file_li_template.setAttribute('class', 'menu_item clickable');
+log_file_li_template.setAttribute('data-url', '/LFS_F2/www/logs.html');
 
-function loadLogTypes(e) {
 
-    var trigger = e.target.parentNode;
+//<li  id="log_1" data-role="log-trigger" class="menu_item clickable" data-url="/LFS_F2/www/logs.html"><a>System</a></li>
+
+function populateLogMenu(parent, logs, depth) {
+
+    for (var iter in logs) {
+        var log = logs[iter];
+        console.log(log);
+        if (log.type == 'folder' && log.childs.length > 0) {
+            //create new parent ul
+            var lili = document.createElement('li');
+            var a = document.createElement('a');
+            a.innerHTML = log.name + '<span class="fa fa-chevron-down"></span>';
+            var new_ul = parent.cloneNode(false);
+            new_ul.id = "log_menu_" + log.name;
+            //call populate with the new ul
+            populateLogMenu(new_ul, log.childs, depth + 1);
+            lili.appendChild(a);
+            lili.appendChild(new_ul);
+            parent.appendChild(lili);
+        } else if (log.type == 'file') {
+            // clone li template
+            var new_li = log_file_li_template.cloneNode(true);
+            if (iter == 0 && depth > 0) {
+                new_li.classList.add('sub_menu');
+            }
+            // mod id and url
+            new_li.id = log.name;
+            new_li.setAttribute('data-data', log.path);
+            new_li.innerHTML = '<a>' + log.name + '</a>';
+            // append new li to parent
+            parent.appendChild(new_li);
+        }
+    }
+
+}
+
+var setContentHeight = function () {
+    // reset height
+    $RIGHT_COL.css('min-height', $(window).height());
+
+    var bodyHeight = $BODY.outerHeight(),
+        footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
+        leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+        contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
+
+    // normalize content
+    contentHeight -= $NAV_MENU.height() + footerHeight;
+
+    $RIGHT_COL.css('min-height', contentHeight);
+};
+
+function loadLogTypes() {
 
     $.ajax({
         type: "GET",
         //url: '/cgi-bin/getLogsAvailable.sh', //TODO activate this when script exists
-        url: '/LFS_F2/www/menu_example.html',
+        url: '/LFS_F2/www/menu_example.json',
         accepts:{
-            html: 'text/html'
+            html: 'application/json'
         },
         context: this,
-        dataType: "html",
+        dataType: "json",
 
         // html retrieve call was *not* successful
         error: function() {
@@ -25,23 +79,52 @@ function loadLogTypes(e) {
         // html retrieve call was successful
         //insert it on the main div. Since we just loaded the html we do not have to remove any content since its empty
         success: function(response_payload){
-            document.getElementById('log_type_menu').innerHTML = response_payload;
-
+            console.log(response_payload);
+            var parent_dude = document.getElementById('log_type_menu');
+            //unpopulate parent
+            while (parent_dude.firstChild) {
+                parent_dude.removeChild(parent_dude.firstChild);
+            }
+            //populate parent
+            populateLogMenu(parent_dude, response_payload, 0);
+            //set onclick on new menu items
             $('.menu_item.clickable').click(setMainContent);
+
+            $('#log_type_menu').find('a').on('click', function(ev) {
+                var $li = $(this).parent();
+
+                if ($li.is('.active')) {
+                    $li.removeClass('active active-sm');
+                    $('ul:first', $li).slideUp(function() {
+                        setContentHeight();
+                    });
+                } else {
+                    // prevent closing menu if we are on child menu
+                    if (!$li.parent().is('.child_menu')) {
+                        $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                        $SIDEBAR_MENU.find('li ul').slideUp();
+                    }
+
+                    $li.addClass('active');
+
+                    $('ul:first', $li).slideDown(function() {
+                        setContentHeight();
+                    });
+                }
+            });
+
         },
 
         failure: function () {
             $.notify("could not get available logs", "error");
         }
     });
-
-    $('#'+trigger.id).toggleClass('non-toggled toggled');
-
 }
 
 function setMainContent(e){
 
     var trigger = e.target.parentNode;
+
     console.log(trigger);
     //make ajax request to obtain content of the dashboard (home screen)
     $.ajax({
@@ -64,8 +147,8 @@ function setMainContent(e){
             document.getElementById('main_wrapper').innerHTML = response_payload;
             $('.current-page').toggleClass('current-page');
             $('#'+trigger.id).toggleClass('current-page');
-            if ( trigger.get('data-role') == 'log-trigger' ) {
-
+            if ( trigger.getAttribute('data-role') == 'log-trigger' ) {
+                window.path_of_selected_log = trigger.getAttribute('data-data');
             }
             $('.to_eval').each(function(){
                 $.getScript( $(this).attr('src'), function( data, textStatus, jqxhr ) {
@@ -86,8 +169,6 @@ function setMainContent(e){
 
 }
 
-$('.log_menu.non-toggled').click(loadLogTypes);
-$('.log_menu.toggled').click(function () {
-    $('.log_menu.toggled').toggleClass('toggled non-toggled');
-});
 $('.menu_item.clickable').click(setMainContent);
+
+loadLogTypes();
