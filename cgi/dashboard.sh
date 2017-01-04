0ff356c260874adc5e903c608ecea7d4104435ed
Guillermo Dobao <ga2468@hotmail.com>
2017-01-04 21:00:58 +0100

    modifed deploy script to support wan upload

--- cgi/dashboard.sh
+++ cgi/dashboard.sh
@@ -5,36 +5,37 @@ OIFS="$IFS"
 
 cpu=`top -n 1 | awk 'NR == 3 {print $2 "$" $6 "$" $4 "$" $10 "$" $12 "$" $14 "$" $16 "$" $8 }'`
 IFS="$"
-set $cpu
+cpu_array=($cpu)
 #Args="$*"
 IFS="$OIFS"
 
+echo "Status: 200 Ok"
 echo "Content-Type: application/json"
 echo ""
 echo "{"
 echo "  \"cpu\":["
-echo "      $1,"            #cpu_usr
-echo "      $2,"            #cpu_nice
-echo "      $3,"            #cpu_sys
-echo "      $4,"            #cpu_iowait
-echo "      $5,"            #cpu_irq
-echo "      $6,"            #cpu_soft
-echo "      $7,"            #cpu_steal
-echo "      $8"             #cpu-idle
+echo "      ${cpu_array[0]},"            #cpu_usr
+echo "      ${cpu_array[1]},"            #cpu_nice
+echo "      ${cpu_array[2]},"            #cpu_sys
+echo "      ${cpu_array[3]},"            #cpu_iowait
+echo "      ${cpu_array[4]},"            #cpu_irq
+echo "      ${cpu_array[5]},"            #cpu_soft
+echo "      ${cpu_array[6]},"            #cpu_steal
+echo "      ${cpu_array[7]}"             #cpu-idle
 echo "  ],"
 
 mem=`free -m | awk 'NR == 2 {print $3 "$" $4 "$" $6 "$" $5 "$" $7 }'`
 IFS="$"
-set $mem
+mem_array=($mem)
 #Args="$*"
 IFS="$OIFS"
 
 echo "  \"mem\":["
-echo "      $1,"            #mem_used
-echo "      $2,"            #mem_free
-echo "      $3,"            #mem_cache
-echo "      $4,"            #mem_shared
-echo "      $5"             #mem_avail
+echo "      ${mem_array[0]},"            #mem_used
+echo "      ${mem_array[1]},"            #mem_free
+echo "      ${mem_array[2]},"            #mem_cache
+echo "      ${mem_array[3]},"            #mem_shared
+echo "      ${mem_array[4]}"             #mem_avail
 echo "  ],"
 
 disk=`df 2>/dev/null | awk '{if($6=="/") print $5 }' | sed -e 's/%//g'`
@@ -44,8 +45,8 @@ hostname=`hostname`
 uptime_since=`uptime -s`
 uptime_for=`uptime -p`
 
-active_users=`uptime | awk -F "," '{print $3}'`
-load_average_15m=`uptime | awk '{print $8 "." $9}'`
+active_users=`uptime | awk -F "," '{print $2}'`
+load_average_15m=`uptime | awk -F ',' '{print $8 "." $9}'`
 
 strings=`last -F | head -n 10 | awk '{if($1!="reboot" && $1!="wtmp" && $0!="") if( $2 ~ /pts/ ){if($9=="-"){print $1 "$remote$" $3 "$" $6 "_" $5 "_" $8 "_" $7 "$" $12 "_" $11 "_" $14 "_" $13}else{print $1 "$remote$" $3 "$" $6 "_" $5 "_" $8 "_" $7 "$-"}}else{if( $3 ~ /:/ ) {if($9=="-"){print $1 "$local$" $2 "$$" $6 "_" $5 "_" $8 "_" $7 "$" $12 "_" $11 "_" $14 "_" $13}else{print $1 "$local$" $2 "$" $6 "_" $5 "_" $8 "_" $7 "$-"}}else if($8=="-"){print $1 "$local$" $2 "$" $5 "_" $4 "_" $7 "_" $6 "$" $11 "_" $10 "_" $13 "_" $12}else{print $1 "$local$" $2 "$" $5 "_" $4 "_" $7 "_" $6 "$-"}}}'`
 
@@ -62,16 +63,16 @@ lines=`echo "$strings" | wc -l`
 i=1;
 for string in ${strings}; do
     IFS="$"
-    set $string
+    con=($string)
     #Args="$*"
 
-    when=`echo $4 | sed -e 's/_/ /g'`
-    until=`echo $5 | sed -e 's/_/ /g'`
+    when=`echo ${con[3]} | sed -e 's/_/ /g'`
+    until=`echo ${con[4]} | sed -e 's/_/ /g'`
 
     if [ $i -eq $lines ]; then
-        echo "      {\"user\":\"$1\", \"type\":\"$2\", \"from\":\"$3\", \"when\":\"$when\", \"until\":\"$until\"}"
+        echo "      {\"user\":\"${con[0]}\", \"type\":\"${con[1]}\", \"from\":\"${con[2]}\", \"when\":\"$when\", \"until\":\"$until\"}"
     else
-        echo "      {\"user\":\"$1\", \"type\":\"$2\", \"from\":\"$3\", \"when\":\"$when\", \"until\":\"$until\"},"
+        echo "      {\"user\":\"${con[0]}\", \"type\":\"${con[1]}\", \"from\":\"${con[2]}\", \"when\":\"$when\", \"until\":\"$until\"},"
     fi
     IFS="$OIFS"
 
