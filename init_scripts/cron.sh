a9e416a8ba89a39a1be8f52dcb9eebb5f3e641dc
Adrià Mallorqui <ls28953@alleurl.edu>
2017-01-04 19:02:25 +0100

    Cron manager works fine on Ubuntu

--- init_scripts/cron.sh
+++ init_scripts/cron.sh
@@ -5,8 +5,9 @@ case $1 in
         [[ ! -p /web_server/fifos/cron/request && ! -z `ps -aux | grep /web_server/daemons/cron_manager.sh` ]] || (echo "Daemon already started. Please stop it and start it again" && exit 1)
         mkdir -p /web_server/fifos/cron/
         mkfifo /web_server/fifos/cron/request
+        mkdir -p /web_server/tmp/cron/
         chown www-data:www-data -R /web_server/fifos/cron
-        echo "Starting user manager daemon..."
+        echo "Starting cron manager daemon..."
         #touch /web_server/daemons/cron_manager_daemon_log
         #chmod 777 /web_server/daemons/cron_manager_daemon_log
         #/web_server/daemons/cron_manager.sh /web_server/fifos/cron/ >> /web_server/daemons/cron_manager_daemon_log 2>> /web_server/daemons/cron_manager_daemon_log &
@@ -15,9 +16,10 @@ case $1 in
         ;;
     stop)
         echo "Signaling daemon..."
-        echo '2$$$$$$$$$' >> /web_server/fifos/cron/request
+        echo '2$$$$$$$$$$$' >> /web_server/fifos/cron/request
         echo "Waiting for on-going requests to end..."
         rm -f /web_server/fifos/cron/*
+        rm -f /web_server/tmp/cron/*
         echo "Cron manager daemon gracefully shut down."
         ;;
     *)
