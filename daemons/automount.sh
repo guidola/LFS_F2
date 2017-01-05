b7b8bec595cf1c7bb4425a36b10aebd6d3476408
Adrià Mallorqui <ls28953@alleurl.edu>
2017-01-05 19:58:21 +0100

    skel

--- daemons/automount.sh
+++ daemons/automount.sh
@@ -4,4 +4,11 @@ mounted=0
 while true
 do
     usbs=`ls /dev/ | grep -e "sd[b-z]"`
+    for usb in usbs
+    do
+        ismounted=`df | grep /dev/${usb}`
+        if [[ -z ${ismounted} ]]; then
+            mount
+        fi
+    done
 done
