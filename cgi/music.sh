faabf652b0c9a3b96863cd41b6514d8497b72062
Adrià Mallorqui <ls28953@alleurl.edu>
2017-01-05 16:55:07 +0100

    music CGI and daemon implemented

---
+++ cgi/music.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+die() {
+    logger -p local0.notice "CGI music: bad request"
+    echo "Status: $1"
+    echo""
+    exit 0
+}
+
+urldecode(){
+  echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x/g;')"
+}
+
+
+[[ $REQUEST_METHOD -eq "POST" ]] || die "400 Bad Request"
+
+#IFS="$"
+play=0
+pause_resume=1
+next=2
+previous=3
+random=4
+unrandom=5
+repeat=6
+unrepeat=7
+xcorrect=0
+xerror=1
+esyntax=2
+ecode=3
+
+[[ $CONTENT_LENGTH -eq 0 ]] || read -n $CONTENT_LENGTH url
+url="${url}&"
+ACTION=`echo ${url} | grep -oP '(?<=action=).*?(?=&)' | urldecode`
+SONG=`echo ${url} | grep -oP '(?<=song=).*?(?=&)' | urldecode`
+
+
+#verify we got all params we need.
+[[ ! -z  $ACTION ]] || ACTION=${unrandom}
+#[[ ! -z $SONG ]] || die "400 Bad Request"
+
+
+#create return fifo
+ret_fifo="/web_server/fifos/music/$$"
+mkfifo $ret_fifo
+#echo "FIFO ${ret_fifo} created"
+
+#send process request to process manager daemon
+echo "1\$$$\$$ACTION\$$SONG" >> /web_server/fifos/acl/request
+#echo "Echo to request fifo done --> 1\$$$\$$ACTION\$$SONG"
+#wait for response from the authentication daemon
+read resp_code < $ret_fifo
+#echo "Read from return fifo done --> .${resp_code}."
+echo "Content-Type: application/json"
+
+case $ACTION in
+    ${play})
+        logger -p local0.notice "CGI music: play song ${SONG} request"
+         ;;
+    ${pause_resume})
+        logger -p local0.notice "CGI music: pause or resume song request"
+         ;;
+    ${next})
+        logger -p local0.notice "CGI music: next song request"
+         ;;
+    ${previous})
+        logger -p local0.notice "CGI music: previous song request"
+         ;;
+    ${random})
+        logger -p local0.notice "CGI music: random list request"
+         ;;
+    ${unrandom})
+        logger -p local0.notice "CGI music: original list request"
+         ;;
+    ${repeat})
+        logger -p local0.notice "CGI music: repeat song request"
+         ;;
+    ${unrepeat})
+        logger -p local0.notice "CGI music: unrepeat song request"
+         ;;
+esac
+
+if [[ ! -z $resp_code ]]; then
+    case $resp_code in
+        ${esyntax})
+            echo "Status: 500 Internal Server Error"
+            echo ""
+            echo "\"Oops. Syntax error\""
+            logger -p local0.notice "CGI music: internal error (syntax error)"
+            ;;
+        ${ecode})
+            echo "Status: 500 Internal Server Error"
+            echo ""
+            echo "\"Oops. The requested action does not exist\""
+            logger -p local0.notice "CGI music: internal error (wrong action)"
+            ;;
+        ${xerror})
+            echo "Status: 200 OK"
+            echo ""
+            echo '{"rc": false}'
+            logger -p local0.notice "CGI music: error, the action could not be completed"
+            ;;
+        ${xcorrect})
+            echo "Status: 200 OK"
+            echo ""
+            echo "Dobao is the JS king" >> "${ret_fifo}"
+            read response < $ret_fifo
+            #echo "second read done"
+            echo "{\"rc\":true, \"payload\": ${response}}"
+            #NO s'ha de fer cas del current si s'ha demanat un repeat
+            logger -p local0.notice "CGI music: request success"
+            ;;
+    esac
+fi
---
+++ daemons/music_player.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+
+getlist() {
+
+    info=`cat /web_server/music/playlist.txt`
+    missatge="{\"list\": ["
+    i=1
+    current=0
+    for line in $info
+    do
+        missatge="${missatge}\"${line}\","
+        if [[ ${line} == ${song} ]]; then
+            current=${i}
+        fi
+        let i=i+1
+    done
+    missatge="${missatge%?}], \"current\": ${current}}"
+}
+
+play=0
+pause_resume=1
+next=2
+previous=3
+random=4
+unrandom=5
+repeat=6
+unrepeat=7
+xcorrect=0
+xerror=1
+esyntax=2
+ecode=3
+OIFS="$IFS"
+
+while true
+do
+    #echo "Going to read from --> ${1}request"
+    logger -p local1.notice "music player daemon: waiting for requests"
+    IFS="$" read codi pid action song < ${1}request
+    IFS="$OIFS"
+    [[ ${codi} -ne "2" ]] || exit 0
+    #echo "passed through first condition"
+    [[ ! -z ${codi} && ! -z ${pid} && ${pid} != *"$"* ]] || (echo "${esyntax}" >> "${1}${pid}" & continue)
+    #echo "passed through the conditions, going to answer to --> ${1}${pid} and the action is ${action}"
+    case ${action} in
+        ${play})
+            #echo "entered to play song"
+            logger -p local1.notice "music player daemon: play song ${song} request received"
+            echo "S" > "${1}mpg123_fifo"
+            echo "L /media/usb/music/${song}" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: play song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: play song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+        ${pause_resume})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: pause/resume song request received"
+            echo "P" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: pause/resume song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: pause/resume song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+	    ${next})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: next song request received"
+            current=`cat /web_server/music/playlist.txt | grep -n ${song} | awk -F: '{print $1}'`
+            total=`wc -l /web_server/music/playlist.txt | awk '{print $1}'`
+            if [[ $current -ge $total ]]; then
+                next=1
+            else
+                let next=current+1
+            fi
+            song=`cat /web_server/music/playlist.txt | awk -v var="$next" 'NR == var'`
+            echo "S" > "${1}mpg123_fifo"
+            echo "L /media/usb/music/${song}" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: next song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: next song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+	    ${previous})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: previous song request received"
+            current=`cat /web_server/music/playlist.txt | grep -n ${song} | awk -F: '{print $1}'`
+            total=`wc -l /web_server/music/playlist.txt | awk '{print $1}'`
+            if [[ $current -le 1 ]]; then
+                previous=current
+            else
+                let previous=current-1
+            fi
+            song=`cat /web_server/music/playlist.txt | awk -v var="$previous" 'NR == var'`
+            echo "S" > "${1}mpg123_fifo"
+            echo "L /media/usb/music/${song}" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: previous song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: previous song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+        ${random})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: random list request received"
+            shuf /web_server/music/playlist.txt > /web_server/music/randomlist.txt
+            cat /web_server/music/randomlist.txt > /web_server/music/playlist.txt
+            song=`cat /web_server/music/playlist.txt | awk 'NR == 1'`
+            echo "S" > "${1}mpg123_fifo"
+            echo "L /media/usb/music/${song}" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: random list request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: random list request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+	    ${unrandom})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: original list request received"
+            cat /web_server/music/originallist.txt > /web_server/music/playlist.txt
+            song=`cat /web_server/music/playlist.txt | awk 'NR == 1'`
+            echo "S" > "${1}mpg123_fifo"
+            echo "L /media/usb/music/${song}" > "${1}mpg123_fifo"
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: original list request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: original list request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+	    ${repeat})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: repeat song request received"
+            touch /web_server/music/repeatlist.txt
+            for i in `seq 1 30`
+            do
+                echo "${song}" >> /web_server/music/repeatlist.txt
+            done
+            cat /web_server/music/repeatlist.txt > /web_server/music/playlist.txt
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: repeat song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: repeat song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+	    ${unrepeat})
+            #echo "entered to pause/resume song"
+            logger -p local1.notice "music player daemon: unrepeat song request received"
+            rm /web_server/music/repeatlist.txt
+            cat /web_server/music/originallist.txt > /web_server/music/playlist.txt
+            echo "${xcorrect}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: unrepeat song request succeeded, waiting for response"
+            #echo "asnwered with xcorrect to --> ${1}${pid}"
+            read brossa < ${1}${pid}
+            getlist
+            echo "${missatge}" >> "${1}${pid}"
+            logger -p local1.notice "music player daemon: unrepeat song request finished, information send to CGI"
+	        #echo "asnwered with the message to --> ${1}${pid}"
+	        #echo "the message is: ${missatge}"
+	        ;;
+
+        *)
+	    #echo "code error, going to answer to --> ${1}${pid}"
+	    echo "${ecode}" >> "${1}${pid}"
+	    #echo "answered done"
+	    ;;
+    esac
+done
